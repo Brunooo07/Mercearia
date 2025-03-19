5aa5f305638dcffbfa6c1fda4b5ff2ae8676d8ff
Bruno Ferreira <134228992+Brunooo07@users.noreply.github.com>
2025-03-19 14:25:47 +0000

    Update

--- mvc/controller.py
+++ mvc/controller.py
@@ -19,5 +19,43 @@ class ControllerCategoria:
             print('A categoria já existe')
     def removerCategoria(self, categoriaRemover):
          x = DaoCategoria.ler()
-         cat = filter()
+         cat = list(filter(lambda x: x.categoria == categoriaRemover, x))
+         if len(cat) <= 0:
+              print('Essa categoria não existe')
+         else:
+              for i in range(len(x)):
+                   if x[i].categoria == categoriaRemover:
+                        del x[i]
+                        break
+              print('Categoria removida com sucesso')
 
+              with open('categoria.txt', 'w') as arq:
+                   for i in x:
+                        arq.writelines(i.categoria)
+                        arq.writelines('\n')
+    def alterarCategoria(self, categoriaAlterar, categoriaAlterada):
+         x = DaoCategoria.ler()
+         cat = list(filter(lambda x: x.categoria == categoriaAlterar, x))
+         if len(cat) > 0:
+              cat1 = list(filter(lambda x: x.categoria == categoriaAlterada, x))
+              if len(cat1) == 0:
+               x = list(map(lambda x: Categoria(categoriaAlterada) if (x.categoria == categoriaAlterar) else(x), x))
+               print(f'A categoria {categoriaAlterar} foi modificada para {categoriaAlterada} ')
+              else:
+                    print(" A categoria para qual deseja alterar já existe")
+         else:
+               print("A categoria que deseja alterar não existe")
+
+         with open('categoria.txt', 'w') as arq:
+              for i in x:
+                   arq.writelines(i.categoria)
+                   arq.writelines('\n')
+    def mostrarCategoria(self):
+         categorias = DaoCategoria.ler()
+         if len(categorias) == 0:
+              print('Não há categorias')
+         else:
+              for i in categorias:
+                   print(i.categoria)
+a = ControllerCategoria()
+a.mostrarCategoria()
